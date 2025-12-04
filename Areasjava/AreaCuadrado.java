24fe0c46ac54b878a1c18164251a3d5344c81652
Angel Martinez Olguin <250110633@itsoeh.edu.mx>
2025-12-04 10:57:43 -0600

    se modifico el archico

--- Areasjava/AreaCuadrado.java
+++ Areasjava/AreaCuadrado.java
@@ -1,5 +1,25 @@
 package Areasjava;
+   import java.util.Scanner;
 
-public class AreaCuadrado {
-    
-}
+ class AreaCuadrado {
+   
+   
+   public static void main(String[] args) {
+   
+   Scanner sc = new Scanner(System.in);
+   
+   sc.useLocale(Locale.US);
+   
+
+   System.out.println( "ingresa el valor del lado ");
+   
+
+   
+   double lado = sc.nextDouble();
+   
+   double area =lado er0+''* lado;
+   
+
+   System.out.println("El area del cuadrado es: );
+   }
+   }
--- Areasjava/AreaTriangulo.java
+++ Areasjava/AreaTriangulo.java
@@ -1,5 +1,28 @@
 package Areasjava;
+   import java.util.Scanner;
 
 public class AreaTriangulo {
-    
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Pedir la base
+        System.out.print("Ingresa la base del triángulo: ");
+        double base = scanner.nextDouble();
+
+        // 2. Pedir la altura
+        System.out.print("Ingresa la altura del triángulo: ");
+        double altura = scanner.nextDouble();
+
+        // 3. Calcular el área
+        // Usamos double para permitir decimales
+        double area = (base * altura) / 2;
+
+        // 4. Mostrar el resultado
+        System.out.println("------------------------------");
+        System.out.println("El área del triángulo es: " + area);
+
+        // Cerrar el scanner (buena práctica)
+        scanner.close();
+    }
 }
