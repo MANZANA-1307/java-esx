443b473b33eb3b054a0c7a1c3d03f9f40e8abaa3
Angel Martinez Olguin <250110633@itsoeh.edu.mx>
2025-12-04 11:41:39 -0600

    se modifico el archico

---
+++ Areasjava/OpercacionesArea.java
@@ -0,0 +1,17 @@
+package Areasjava;
+
+public class OpercacionesArea {
+ public static double MultiplicaionCuadrado(double lado , double area){
+    return area = lado + lado;
+ }    
+ public static double MultiplicacionCirculo(double area , double radio){
+    return area= Math.PI * Math.pow(radio,2);
+ }
+    public static double OpercionTrinagulo(double base, double altura , double area){
+    return area = base * altura / 2 ;
+ }
+     public static  double OprecionesHexagono(double perimetro, double l , double apotema , double area){
+    return perimetro = 6 * l;
+ }
+
+}
